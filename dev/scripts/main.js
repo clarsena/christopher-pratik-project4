215504c9a7df9a814053e636e446208fcae6f327
Christopher Arsenault <christopher.arsenault82@gmail.com>
2018-08-27 09:24:09 -0400

    should be completed

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,13 +1,3 @@
-//  Customer enters their required search parameters (ingredients, course type, cusisine type, dietary restrictions)
-//  Example of a search for chicken and broccoli / Main dish / Italian / No dietary restriction
-//  Do an API call for all recipes with chicken and broccoli
-//  Then filter the results based on the other criteria - starting with course type - then cuisine type - then dietary restrcitions
-//  The data is pulled from the API and displayed onto the page in a list
-//  The filtered results will then be broken down into individual recipes - displaying an image of the dish, the name of the dish and a  description of it
-//  need to store recipe id from each filtered result and do a separate pull to get the info from there
-//  The recipes will be clickable to take them to a full view of them
-//  The recipes will be opened in a new tab
-
 //  namespace for the project
 const foodApp = {};
 
@@ -19,6 +9,11 @@ foodApp.totalResultCount = 0;
 
 //  the getAllRecipes method takes in the parameters from the search form and gets the matching data from the API. The results are then stored in the storedResults array
 foodApp.getAllRecipes = (ingredients, courseType, cuisineType, dietary) => {
+
+    // show spinner
+    $(".spinner-overlay").show();
+    $("i.fa-spinner").css('display', 'inline-block');
+
     $.ajax({
         url: `${foodApp.allRecipiesApiURL}${foodApp.apiKey}${courseType}${cuisineType}${dietary}`,
         method: 'GET',
@@ -31,6 +26,10 @@ foodApp.getAllRecipes = (ingredients, courseType, cuisineType, dietary) => {
         }
     })
         .then((result) => {
+            // hide spinner
+            $("i.fa-spinner").hide();
+            $(".spinner-overlay").hide();
+
             foodApp.storedResults = [];
             foodApp.pagedResults = [];
             foodApp.recipePages = 0;
@@ -45,8 +44,8 @@ foodApp.getAllRecipes = (ingredients, courseType, cuisineType, dietary) => {
 
 //  the splitRecipes method splits the intially stored results into an array of results pages, with 21 entries on each
 foodApp.splitRecipes = () => {
-    for (let i = 0; i < foodApp.storedResults.length; i += 21) {
-        const block = foodApp.storedResults.slice(i, i + 21);
+    for (let i = 0; i < foodApp.storedResults.length; i += 18) {
+        const block = foodApp.storedResults.slice(i, i + 18);
         foodApp.pagedResults.push(block);
     }
 }
@@ -65,15 +64,28 @@ foodApp.displayRecipes = (recipes) => {
         foodApp.getSingleRecipe(item.id);
     });
     //  only show the show previous button if there are results to go back to
-    if(foodApp.recipePages > 0) {
+    if (foodApp.recipePages > 0) {
         const showPreviousButton = `<button class="show-previous show-button">Show Previous Results</button>`;
         $('.page-results-container').append(showPreviousButton);
     }
     //  only show the show more button if there are still more results to show
-    if(foodApp.recipePages <= ((foodApp.pagedResults.length) - 2)) {
+    if (foodApp.recipePages <= ((foodApp.pagedResults.length) - 2)) {
         const showMoreButton = `<button class="show-more show-button">Show More Results</button>`;
         $('.page-results-container').append(showMoreButton);
     }
+    $('footer').empty();
+    $('footer').append(`<h4>Created by Christopher Arsenault & Pratik Gauchan - chrisPratt Codes &copy; 2018</h4>`);
+}
+
+//  the rating method converts the numerical rating (if present) and displays stars in its place
+foodApp.rating = (ratingNum) => {
+    let tempRating = '';
+    if (ratingNum) {
+        for (let i = 1; i <= ratingNum; i++) {
+            tempRating += `<span class="star"><i class="fas fa-star"></i></span>`;
+        }
+    }
+    return tempRating;
 }
 
 //  the getSingleRecipe method takes in a recipeID and pulls the info for that specific recipe
@@ -93,35 +105,50 @@ foodApp.getSingleRecipe = (recipeID) => {
             if (result.attributes.cuisine) {
                 cuisines = result.attributes.cuisine.join(', ');
             }
+            const rating = foodApp.rating(result.rating);
+
             //  create the HTML elements to write the recipe to the DOM and append it to the recipe-list div
-            const showRecipe = `<a href="${result.source.sourceRecipeUrl}" target="top"><div class="recipe-container">
-        <div class="img-container"><img src='${result.images[0].hostedLargeUrl}'></div>
-        <h2>${result.name}</h2>
-        <h3>Rating: ${result.rating} / 5</h3>
-        <h3>Total Time to Prepare: ${result.totalTime}</h3>
-        <h3>Number of Servings: ${result.numberOfServings}</h3>
-        <h3>Course Types: ${courses}</h3>
-        <h3>Cuisine Types: ${cuisines}</h3>
-        
-        </div><div class="recipe-overlay"><h3>Click here to read the full recipe</h3></div></a>`
-        $('.recipe-list').append(showRecipe);
-    });
+            const showRecipe = `
+            <a href="${result.source.sourceRecipeUrl}" target="top">
+                <div class="recipe-container">
+                    <div class="img-container">
+                        <img src='${result.images[0].hostedLargeUrl}'>
+                    </div>
+                
+                    <h2>${result.name}</h2>
+                    <h4>Rating: ${rating}</h4>
+                    <h3>Total Time to Prepare: ${result.totalTime}</h3>
+                    <h3>Number of Servings: ${result.numberOfServings}</h3>
+                    <h3>Course Types: ${courses}</h3>
+                    <h3>Cuisine Types: ${cuisines}</h3>
+                </div>
+
+                <div class="recipe-overlay">
+                    <h3>Click here to read the full recipe</h3>
+                </div>
+            </a>`
+            $('.recipe-list').append(showRecipe);
+        });
 }
 
 //  the events method will hold general event listeners for the site
 foodApp.events = () => {
+
     $('.initial-search').on('submit', function (e) {
         e.preventDefault();
-        const ingredients = $('input[type=text]').val();
+        const ingredients = $('.initial-search-box').val();
         $('.main-welcome-page').hide();
         $('nav').show();
         $('.recipe-search-box').val($('.initial-search-box').val());
+
         foodApp.getAllRecipes(ingredients, '', '', '');
+
     });
-    $('.recipe-search').on('submit', function (e) {
-        e.preventDefault();
+
+    $('.submit button').on('click', function (e) {
+
         //  store the results from the form to be used later for pagination
-        const ingredients = $('input[type=text]').val();
+        const ingredients = $('.recipe-search-box').val();
         const courses = $('input[name=course-type]:checked').val();
         const cuisines = $('input[name=cuisine-type]:checked').map(function () {
             return $(this).val();
@@ -129,20 +156,74 @@ foodApp.events = () => {
         const dietary = $('input[name=dietary-restrictions]:checked').val();
         //  send the search results to the getAllRecipes method to pull the data from the API
         foodApp.getAllRecipes(ingredients, courses, cuisines, dietary);
+
     });
+
     //  event listener to clear the search form
     $('.form-reset').on('click', function () {
         $('.recipe-search').trigger('reset');
     })
+
     //  event listener for the show previous button to show previous recipe results
     $('body').on('click', '.show-previous', function () {
         foodApp.recipePages--;
         foodApp.displayRecipes(foodApp.pagedResults[foodApp.recipePages]);
     });
+
     //  event listener for the show more button to show more recipe results
     $('body').on('click', '.show-more', function () {
         foodApp.recipePages++;
         foodApp.displayRecipes(foodApp.pagedResults[foodApp.recipePages]);
+    });
+
+    // event listener than hides all the sub-menu options by default
+    $('html').on('click', function () {
+        $(".sub-menu").hide();
+    });
+
+    // event listener that prevents sub-menu hiding if clicked inside the main-menu-options div
+    $('.main-menu-options').on('click', function (e) {
+        e.stopPropagation();
+    });
+
+    //  event listener for showing/hiding sub-menu of only course type while hiding other sub-menus
+    $('.course-type button').on('click', function (e) {
+        $(".spinner-overlay").toggle();
+        $(".course-type .sub-menu").toggle();
+        $(".cuisine-type .sub-menu").hide();
+        $(".dietary-restrictions .sub-menu").hide();
+        e.preventDefault();
+    });
+
+    //  event listener for showing/hiding sub-menu of only cuisine type while hiding other sub-menus
+    $('.cuisine-type button').on('click', function (e) {
+        $(".spinner-overlay").toggle();
+        $(".cuisine-type .sub-menu").toggle();
+        $(".course-type .sub-menu").hide();
+        $(".dietary-restrictions .sub-menu").hide();
+        e.preventDefault();
+    })
+
+    //  event listener for showing/hiding sub-menu of only dietary restrictions while hiding other sub-menus
+    $('.dietary-restrictions button').on('click', function (e) {
+        $(".spinner-overlay").toggle();
+        $(".dietary-restrictions .sub-menu").toggle();
+        $(".cuisine-type .sub-menu").hide();
+        $(".course-type .sub-menu").hide();
+        e.preventDefault();
+    })
+
+    // empty array that stores selected cuisine type
+    let selectedHolder = [];
+
+    $(".sub-menu input[type=checkbox]").on('click', function () {
+        let selectedValue = $(this).val();
+        // filters through the array and takes out cuisine types that are in the array and have been clicked on again
+        if (selectedHolder.includes(selectedValue)) {
+            selectedHolder = selectedHolder.filter(selected => selected != selectedValue);
+        } else {
+            selectedHolder.push(selectedValue);
+        }
     });
 }
 
